259b0de1493ef9f7a9410b8d0950a99385330238
timothyisaiah <timothyisaiah7@gmail.com>
2025-04-15 01:10:35 +0300

    CORS added patch

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   app.enableCors({
     origin: 'http://localhost:3000', // 👈 allow frontend
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
   await app.listen(process.env.PORT ?? 3001);
